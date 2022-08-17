95574b5127c1b43b3bfa8f13464fed8d01620af8
MohanapriyaDass <89187739+MohanapriyaDass@users.noreply.github.com>
2022-08-17 19:36:28 +0530

    Create DbUtilDatabase.java

---
+++ Assisted 2/StoredProcedure/DbUtilDatabase.java
@@ -0,0 +1,30 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DbUtilDatabase {
+	public static final String DRIVER_CLASS="com.mysql.jdbc.Driver";
+	public static final String DB_URL="jdbc:mysql://localhost:3306/ecommerce";
+	public static final String USERNAME="root";
+	public static final String PASSWORD="Dhoni7@#";
+	
+	
+public static Connection getCon() throws ClassNotFoundException, SQLException {
+	//register the driver
+	Class.forName(DRIVER_CLASS);
+	Connection con=DriverManager.getConnection(DB_URL,USERNAME,PASSWORD);
+	return con;
+}
+
+public void checkConnection() throws ClassNotFoundException, SQLException{
+	Connection con=DbUtilDatabase.getCon();
+	if(con!=null) {
+		System.out.println("your connection is established");
+	}
+	else {
+		System.out.println("Oops...connection failed");
+	}
+    
+}
+
+}
