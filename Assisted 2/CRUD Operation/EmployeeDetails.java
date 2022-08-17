1c8d9433ee784cd3c09f927b3cc4370a3bcc5895
MohanapriyaDass <89187739+MohanapriyaDass@users.noreply.github.com>
2022-08-17 19:09:59 +0530

    Create EmployeeDetails.java

---
+++ Assisted 2/CRUD Operation/EmployeeDetails.java
@@ -0,0 +1,32 @@
+
+public class EmployeeDetails {
+private int Eid;
+private String Ename;
+private float Esalary;
+private String Eemail;
+public int getEid() {
+	return Eid;
+}
+public void setEid(int eid) {
+	Eid = eid;
+}
+public String getEname() {
+	return Ename;
+}
+public void setEname(String ename) {
+	Ename = ename;
+}
+public float getEsalary() {
+	return Esalary;
+}
+public void setEsalary(float esalary) {
+	Esalary = esalary;
+}
+public String getEemail() {
+	return Eemail;
+}
+public void setEemail(String eemail) {
+	Eemail = eemail;
+}
+
+}
